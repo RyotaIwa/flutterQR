cac0f73c2ae7ff4571019a4e8d0a6a77d07ec5e4
RyotaIwa <158148644+RyotaIwa@users.noreply.github.com>
2026-03-12 19:12:21 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
     }
 
     buildTypes {
-                release {
+               release {
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
